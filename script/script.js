35128cb7760b20e315eb6229b281b134ebdd4f46
Paul Lee <pauljlee94@gmail.com>
2019-04-10 09:53:39 -0400

    Add score system

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
-// function playerPlay() {
-//     let input = prompt("Rock, paper, or scissors?")
-//     return input.toLowerCase()
-// }
+let counter = 0
+let playerScore = 0;
+let computerScore = 0;
+const playerScoreBoard = document.querySelector('.playerScore')
+const computerScoreBoard = document.querySelector('.computerScore')
 
 function computerPlay() {
     let choices = ['rock','paper','scissors']
@@ -9,69 +10,61 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let result
     if (playerSelection == "rock") {
         (computerSelection == "rock") ? result = "T" : 
-        (computerSelection == "paper") ? result = "L" :
-        result = "W"
-    }   else if (playerSelection == "paper") {
-        (computerSelection == "rock") ? result = "W" : 
+        (computerSelection == "paper") ? computerScore++ :
+        playerScore++
+    }else if (playerSelection == "paper") {
+        (computerSelection == "rock") ? playerScore++ : 
         (computerSelection == "paper") ? result = "T" :
-        result = "L"
-    }   else if (playerSelection == "scissors") {
-        (computerSelection == "rock") ? result = "L" : 
+        computerScore++
+    }else if (playerSelection == "scissors") {
+        (computerSelection == "rock") ? computerScore++ : 
         (computerSelection == "paper") ? result = "T" :
-        result = "W"
-    }   else {
-        result = "undefined"
-        }
-    return result
+        playerScore++
+    }   
 }
 
-// function game () {
-//     let counter = 0
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     while (counter < 5) {
-//         let result = playRound(playerPlay(), computerPlay())
-//         if (result == "W") {
-//             playerScore++
-//             alert("You:" + playerScore + "\n"  + "Computer:" + computerScore)
-//             counter++
-//         }   else if (result == "L") {
-//             computerScore++
-//             alert("You:" + playerScore + "\n" + "Computer:" + computerScore)
-//             counter++
-//         }   else if (result == "T"){
-//             alert("Tie")
-//             counter++
-//         }   else {
-//             alert("Invalid entry")
-//         }
+// Score Section
+function game () {
+    if (playerScore < 5 && computerScore < 5) {
+        playerScoreBoard.textContent = playerScore.toString()
+        computerScoreBoard.textContent = computerScore.toString()
 
-//     }
-//     if (playerScore > computerScore) {
-//         alert("You win!!")
-//     }   else if (playerScore < computerScore) {
-//         alert("You lose :(")
-//     }   else {
-//         alert("Tie")
-//     }
-//     alert("You:" + playerScore + "\n" + "Computer:" + computerScore)
-// }
+    }else if (playerScore == 5) {
+        playerScoreBoard.textContent = playerScore.toString()
+        
+    }else if (computerScore == 5) {
+        computerScoreBoard.textContent = computerScore.toString()
+        
+    }
+}
 
-
-// game()
-
+function reset () {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreBoard.textContent = 0
+    computerScoreBoard.textContent = 0
+}
+// Choice Buttons
 const rockButton = document.querySelector('#rockButton')
-rockButton.addEventListener('click', playRound("rock", computerPlay))
+rockButton.addEventListener('click', function(){
+    playRound('rock', computerPlay());
+    game();
+});
 
 const paperButton = document.querySelector('#paperButton')
-paperButton.addEventListener('click', function(e) {
-    console.log(e)
-})
+paperButton.addEventListener('click', function(){
+    playRound('paper', computerPlay());
+});
 
 const scissorsButton = document.querySelector('#scissorsButton')
-scissorsButton.addEventListener('click', function(e) {
-    console.log(e)
-})
+scissorsButton.addEventListener('click', function(){
+    playRound('scissors', computerPlay());
+});
+
+const resetButton = document.querySelector('.resetButton')
+resetButton.addEventListener('click', function() {
+    reset();
+
+});
